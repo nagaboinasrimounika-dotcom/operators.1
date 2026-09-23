c0c9576f3bd6e2308dc8c632ff7a6c4c1b8d0970
nagaboinasrimounika-dotcom <nagaboinasrimounika@gmail.com>
2026-09-23 18:46:13 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+radius=float(input("enter the radius of the circle"))
+area=3.14*radius*radius
+print(f"area of the circle is: {area}")
+
+#simple intrest
+
+p=int(input("enter the amount"))
+t=float(input("enter the time period"))
+r=float(input("enter the rate of interest"))
+
+simple_intrest=p*t*r/100
+print(f"simple intrest is: {simple_intrest}")
